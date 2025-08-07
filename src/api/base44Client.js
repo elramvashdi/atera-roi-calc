2dad4d72ad9c0cdbe4a8614c24b64239c1990047
Elram <elramvashdi@gmail.com>
2025-08-07 23:03:19 +0300

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,8 +1,8 @@
-// import { createClient } from '@base44/sdk';
-// // import { getAccessToken } from '@base44/sdk/utils/auth-utils';
+import { createClient } from '@base44/sdk';
+// import { getAccessToken } from '@base44/sdk/utils/auth-utils';
 
-// // Create a client with authentication required
-// export const base44 = createClient({
-//   appId: "684ae5503e92c69eaebde4a5", 
-//   requiresAuth: true // Ensure authentication is required for all operations
-// });
+// Create a client with authentication required
+export const base44 = createClient({
+  appId: "684ae5503e92c69eaebde4a5", 
+  requiresAuth: false // Ensure authentication is required for all operations
+});
